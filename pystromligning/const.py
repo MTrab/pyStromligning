921bb715c7886abf01cbfea190e9a5467fac0e62
Malene Trab <malene@trab.dk>
2025-09-11 11:03:26 +0200

    Switch to prod API

--- pystromligning/const.py
+++ pystromligning/const.py
@@ -1,4 +1,4 @@
 """Consts used in the pyStromligning library."""
 
-# API_URL = "https://stromligning.dk/api"
-API_URL = "https://staging.stromligning.dk/api"  # For testing purposes
+API_URL = "https://stromligning.dk/api"
+# API_URL = "https://staging.stromligning.dk/api"  # For testing purposes
